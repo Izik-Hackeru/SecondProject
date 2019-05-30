0d470fbffbf325a39c879510e8648af4a0d43796
Hackeru <Hackeru@Hackeru2.local>
2019-05-30 20:14:55 +0300

    a

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
+
+import { CdkTableModule } from "@angular/cdk/table";
+import { CdkTreeModule } from "@angular/cdk/tree";
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +14,13 @@ import { AddUserComponent } from './users/add-user/add-user.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { ViewAllUsersComponent } from './view-all-users/view-all-users.component';
-import { LoginComponent } from './users/login/login.component';
+import { LoginComponent, LoginDialogComponent } from './users/login/login.component';
+
+import {
+  MatTableModule,
+  MatDialogModule
+} from '@angular/material';
+
 
 @NgModule({
   declarations: [
@@ -16,15 +28,22 @@ import { LoginComponent } from './users/login/login.component';
     AddComponent,
     AddUserComponent,
     ViewAllUsersComponent,
-    LoginComponent
+    LoginComponent,
+    LoginDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [MatTableModule, MatDialogModule],
+  entryComponents: [
+    LoginDialogComponent
+
+  ]
 })
 export class AppModule { }
--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -1,34 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Inject } from '@angular/core';
 import { MyServerService } from '../../my-server.service';
 import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, AfterViewInit {
 
+
+  @ViewChild('input1') input: ElementRef;
   username: String;
   password: String;
 
-  constructor(private server: MyServerService, private route: Router) { }
+  constructor(private server: MyServerService, private route: Router, public dialog: MatDialog) { }
 
   ngOnInit() {
+
   }
 
   login() {
+    // const dialogRef = this.dialog.open(LoginDialogComponent, {
+    //   width: '500px'
+    // })
+    // dialogRef.afterClosed().subscribe(response => {
+
+    // })
+    // if (5 < 6) {
+    //   return;
+    // }
     this.server.login(this.username, this.password).subscribe(
-      data=>{
-        if(data){
+      data => {
+        if (data) {
           //goto home
           this.route.navigate(['/addUser']);
         }
-      }, error=>{
+      }, error => {
         console.log(error);
         alert('try again');
       }
     )
   }
 
+  ngAfterViewInit() {
+    //alert(this.input);
+    console.log(this.input);
+
+  }
+
+
+
+}
+
+
+@Component({
+  selector: 'app-login-dialog',
+  templateUrl: 'loginDialog.html'
+})
+export class LoginDialogComponent implements OnInit {
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private dialogRef: MatDialogRef<LoginDialogComponent>) {
+
+  }
+
+  ngOnInit() {
+
+  }
 }
